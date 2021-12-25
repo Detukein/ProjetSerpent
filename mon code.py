c1dc1ce564a14bf31e64159c6a0f37b10bcdfabf
Detukein <titouanjacquin91@gmail.com>
2021-12-25 23:29:40 +0100

    Message

--- mon code.py
+++ mon code.py
@@ -4,13 +4,3 @@
 #paragraphe 2 qui est bon
 #paragraphe 3 qui doit être déplacé
 
-#paragraphe 2 qui est bon
-
-
-import random as rdm
-
-for i in range(0, 10):
-    a = rdm.randint(0, 10)
-    print(i, a)
-
-#Coucougnette
